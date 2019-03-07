94879648f8d7561a2c443db3516223e6d008aced
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2019-03-07 23:35:31 +0530

    Modfied m3u8Parser and added more unit tests

--- utils/m3u8parser.go
+++ utils/m3u8parser.go
@@ -12,25 +12,21 @@ func ParseM3u8Content(m3u8Content string,playbackUrl string, playbackUrlData str
     var m3u8Info map[string]string
     var urlFormats = make(map[string]map[string]string)
     for _, line := range strings.Split(m3u8Content, "\n") {
-        //fmt.Printf("\nindex %d, line %s\n",index, line)
+
         if strings.HasPrefix(line, "#EXT-X-STREAM-INF:") {
-            //fmt.Println("inside if")
+
             if m3u8Info == nil {
-                //fmt.Println("infoArray array is null initializing it")
                 m3u8Info = make(map[string]string)
             }
+            
             m3u8InfoCsv := strings.Replace(line, "#EXT-X-STREAM-INF:", "", -1)
-            //fmt.Println("m3u8InfoCsv : ",m3u8InfoCsv)
             m3u8InfoRegex := regexp.MustCompile(`([\w\-]+)\=([\w\-]+|"[^"]*")`)
             
             for _, info := range m3u8InfoRegex.FindAllStringSubmatch(m3u8InfoCsv, -1) {
-                //fmt.Printf("index : %d,\tm3u8Info : %s__%s\n", index, m3u8Info[1], m3u8Info[2]);
                 m3u8Info[info[1]] = info[2]
             }
         } else if strings.Contains(line, ".m3u8") {
-            //fmt.Println("inside else")
-            //fmt.Println("infoArray len : ", len(infoArray));
-            
+
             if m3u8Info != nil {
                 
                 averageBandwidthOrBandwidth := func() int {
@@ -78,14 +74,6 @@ func ParseM3u8Content(m3u8Content string,playbackUrl string, playbackUrlData str
             //do nothing
         }
     }
-    
-    /*for key, value := range urlFormats {
-        fmt.Println("\n\nKey : ", key)
-        fmt.Println("Iterating values....")
-        for infoKey, infoValue := range value {
-            fmt.Println("infoKey : ", infoKey, "infoValue : ", infoValue)
-        }
-    }*/
-    
+
     return urlFormats
 }
